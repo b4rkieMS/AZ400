3871df9e3597286e7a6bbda51c78991face0dfbc
b4rkieMS <b4rkie.tb@gmail.com>
2023-10-20 21:27:09 +0000

    add headers and body

--- counter.py
+++ counter.py
@@ -6,11 +6,14 @@ for count in range(1, 101):
     print(count)
 
 # Send a GET request to Google.com
-response = requests.get("https://www.google.com/barnstaple")
+response = requests.get("https://www.google.com")
 
 # Check if the response status code is 404
 if response.status_code == 404:
-    print("Error 404: Page not found. Nope!")
+    print("Error 404: Page not found")
 else:
-    # Print the status code of the response
+    # Print the status code and headers of the response
     print("Response status code:", response.status_code)
+    print("Response headers:", response.headers)
+    # Print the response body
+    print("Response body:", response.text)
