bd3f045e30ec39476d5884aac5df1a8e0711ecb9
b4rkieMS <b4rkie.tb@gmail.com>
2023-10-20 21:20:17 +0000

    added http request to google.com

--- counter.py
+++ counter.py
@@ -1,6 +1,12 @@
+import requests
+
 # This loop will count from 1 to 100
 for count in range(1, 101):
     # Print the current count
     print(count)
 
-    
+# Send a GET request to Google.com
+response = requests.get("https://www.google.com")
+
+# Print the status code of the response
+print("Response status code:", response.status_code)
